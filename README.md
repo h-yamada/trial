6a183434c9b9f0ac402e98a57d2e56deb5b03e5c
Hirokazu Yamada <h-yamada@mediba.jp>
2017-03-02 11:00:27 +0900

    いろいろ

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Coverage Status](https://coveralls.io/repos/github/h-yamada/trial/badge.svg?branch=master)](https://coveralls.io/github/h-yamada/trial?branch=master)
+
 coveralls test by golang
 
 using goveralls
---
+++ trial_test.go
@@ -0,0 +1,20 @@
+package trial
+
+import "testing"
+
+func TestFizzbuzz(t *testing.T) {
+	testData := map[int]string{
+		1:  "Nothing",
+		2:  "Nothing",
+		3:  "Fizz",
+		4:  "Nothing",
+		5:  "Buzz",
+		15: "FizzBuzz",
+	}
+
+	for key, value := range testData {
+		if res := Fizzbuzz(key); value != res {
+			t.Errorf("not matching:%s:%s", res, value)
+		}
+	}
+}
